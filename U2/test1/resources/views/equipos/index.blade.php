f3cc67b3cd901ec150b90f2ff6baeaf4d7dd10e9
MatiasCarvajalD <127509542+MatiasCarvajalD@users.noreply.github.com>
2023-05-05 11:37:33 -0400

    Add files via upload

---
+++ U2/test1/resources/views/equipos/index.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.master')
+
+@section('contenido-principal')
+     <!-- datos -->
+     <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <h3>Equipos</h3>
+            </div>
+        </div>
+
+        <div class="row">
+            <!-- tabla -->
+            <div class="col-12 col-lg-8 order-last order-lg-first">
+                <table class="table table-bordered table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>N°</th>
+                            <th>Nombre</th>
+                            <th>Entrenador</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($equipos as $num=>$equipo)
+                        <tr>
+                            <td class="align-middle">{{$num+1}}</td>
+                            <td class="align-middle">{{$equipo->nombre}}</td>
+                            <td class="align-middle">{{$equipo->entrenador}}</td>
+                            <td>
+                                <a href="#" class="btn btn-sm btn-danger pb-0" data-bs-toggle="tooltip"
+                                    data-bs-title="Borrar {{$equipo->nombre}}">
+                                    <span class="material-icons">delete</span>
+                                </a>
+                                <a href="#" class="btn btn-sm btn-warning pb-0 text-white" data-bs-toggle="tooltip"
+                                    data-bs-title="Editar {{$equipo->nombre}}">
+                                    <span class="material-icons">edit</span>
+                                </a>
+                                <a href="#" class="btn btn-sm btn-info pb-0 text-white" data-bs-toggle="tooltip"
+                                    data-bs-title="Ver {{$equipo->nombre}}">
+                                    <span class="material-icons">group</span>
+                                </a>
+                            </td>
+                        </tr>
+                        @endforeach
+                        
+                        
+                    </tbody>
+                </table>
+            </div>
+
+            <!-- form agregar equipo -->
+            <div class="col-12 col-lg-4 order-first order-lg-last">
+                <div class="card">
+                    <div class="card-header bg-dark text-white">Agregar Equipo</div>
+                    <div class="card-body">
+                        <form method="POST" action="{{route('equipos.store')}}">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="nombre" class="form-label">Nombre</label>
+                                <input type="text" id="nombre" name="nombre" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="entrenador" class="form-label">Entrenador</label>
+                                <input type="text" id="entrenador" name="entrenador" class="form-control">
+                            </div>
+                            <div class="mb-3 d-grid gap-2 d-lg-block">
+                                <button type="reset" class="btn btn-warning">Cancelar</button>
+                                <button type="submit" class="btn btn-success">Agregar Equipo</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+   
+    
---
+++ U2/test1/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\EquipoController;
+use App\Http\Controllers\JugadoresController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/',[HomeController::class,'index'])->name('home.index');
+Route::get('/login',[HomeController::class,'login'])->name('home.login');
+
+Route::get('/equipos',[EquipoController::class,'index'])->name('equipos.index');
+Route::post('/equipos',[EquipoController::class,'store'])->name('equipos.store');
+
+Route::get('/jugadores',[JugadoresController::class,'index'])->name('jugadores.index');
+Route::post('/jugadores',[JugadoresController::class,'store'])->name('jugadores.store');
+
+
